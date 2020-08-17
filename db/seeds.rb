e2b7cddee56ca603ae09ad34d4ea9d113b2ddbc8
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-17 09:12:56 +0700

    setup devise and user detail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p 'seed start'
+
+if User.count.zero?
+  admin = User.create(email: 'admin@admin.com', password: '123456')
+  admin.build_user_detail(name: 'admin')
+  admin.save
+  admin.admin!
+end
+
+p 'seed done'
