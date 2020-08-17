c9e92b6577f0d9c59a260c660d80d072560ecad6
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-17 09:41:47 +0700

    soft delete

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   belongs_to :user
 
   friendly_id :name, use: :slugged
+  acts_as_paranoid
 
   def should_generate_new_friendly_id?
     slug.blank? || name_changed?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_023113) do
+ActiveRecord::Schema.define(version: 2020_08_17_023901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_08_17_023113) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["slug"], name: "index_products_on_slug", unique: true
     t.index ["user_id"], name: "index_products_on_user_id"
   end
