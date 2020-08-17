460cdc4533afe970315aeae85a5c20483bac8643
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-17 09:45:38 +0700

    wakeup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/secretroom', as: 'rails_admin'
   devise_for :users
   root to: 'welcome#index'
+  namespace :api, defaults: { format: :json } do
+    # post 'login', to: 'auth#login'
+    # delete 'logout', to: 'auth#logout'
+    # post 'register', to: 'auth#register'
+    # get 'user', to: 'auth#user'
+    get 'wakeup', to: 'misc#wakeup'
+  end
 end
